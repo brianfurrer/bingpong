6925facadb100f1a2f3fdb697baf18129630d7d8
kiefferbp <kiefferbp@gmail.com>
2016-02-15 14:45:55 -0600

    add support for bph v1.5.0.11

--- scripts/bp.js
+++ scripts/bp.js
@@ -60,8 +60,8 @@ var stopRunningBingPongFlag = false; // for pausing/stopping
 var bphExtensionID = "cohnfldcnegepfhhfbcgecblgjdcmcka";
 var bphCanaryExtensionID = "omepikidpeoofklbmlidbbhojdhpggfj";
 var bphInstallURL = "https://chrome.google.com/webstore/detail/" + bphExtensionID;
-var bphCompatibleVersions = ["1.5.0.10"];
-var bphLatestVersion = "1.5.0.10";
+var bphCompatibleVersions = ["1.5.0.10", "1.5.0.11"];
+var bphLatestVersion = "1.5.0.11";
 var bphInstalled = false;
 
 // license
