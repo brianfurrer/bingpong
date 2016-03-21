9e28faa5371acdff74b64f830bfc694661e2aaec
kiefferbp <kiefferbp@gmail.com>
2016-03-21 17:08:33 -0500

    bump version to v1.0.0-51

--- modules/status.js
+++ modules/status.js
@@ -1,5 +1,5 @@
 bp.status = (function () { 
-	var DEFAULT_STATUS_TEXT = "Created by <a href=\"http://www.reddit.com/user/kiefferbp\" target=\"_blank\">/u/kiefferbp</a>. v0.21.1-50 (BETA)";
+	var DEFAULT_STATUS_TEXT = "Created by <a href=\"http://www.reddit.com/user/kiefferbp\" target=\"_blank\">/u/kiefferbp</a>. v1.0.0-51 (BETA)";
 	var STATUS_RESET_TIMEOUT = 5;
 	var _statusTimer;
 	
