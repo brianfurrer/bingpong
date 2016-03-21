7ee071eb1efb71ed345f73dcd3e9a389b661a23b
kiefferbp <kiefferbp@gmail.com>
2016-03-21 17:19:49 -0500

    Revert "remove the "Automatically run Bing Pong on each visit" option"
    This reverts commit 22dba9d372dfea3cf35cd07c667ef6c767cd63d3.
    
    # Conflicts:
    #	bp.js
    #	modules/status.js

--- bp.js
+++ bp.js
@@ -1,6 +1,6 @@
 // Source Code for Bing Pong (www.bing-pong.com)
 // Created By Brian Kieffer on 3/24/2013
-// Current version: 1.0.0-51 (3/21/2016)
+// Current version: 1.0.0-52 (3/21/2016)
 
 // constants
 var MS_REQUIRED_TO_SHOW_DOWNLOAD_STATUS = 500;
